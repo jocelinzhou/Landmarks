e584d7e7677a57f65512f9b86b611999e6b6dc3c
Jocelin ZHou <zhoujocelin@gmail.com>
2022-05-13 10:05:29 +0800

    Initial Commit

---
+++ Turtle Rock/Turtle_RockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turtle_RockApp.swift
+//  Turtle Rock
+//
+//  Created by Jocelin Work on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Turtle_RockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
